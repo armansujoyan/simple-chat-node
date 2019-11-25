e0accd46c780906117c5875960a210b327fc8a99
Arman Sujoyan <asujoyan@gmail.com>
2019-11-26 01:17:48 +0400

    Configure routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import http, { Server } from 'http';
 
+import ApiRouter from './routes';
+
 const port: number | string = process.env.PORT || 5000;
 
 const app: Application = express();
@@ -12,5 +14,7 @@ if (process.env.NODE_ENV === 'development') {
   dotenv.config();
   app.use(cors());
 }
+
+app.use(ApiRouter);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));

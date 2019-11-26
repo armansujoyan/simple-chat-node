21d87b4c210389a237c6b378a64713809b589936
Arman Sujoyan <asujoyan@gmail.com>
2019-11-26 12:14:25 +0400

    Add validation method to user model interface.

--- src/interfaces/user.model.interface.ts
+++ src/interfaces/user.model.interface.ts
@@ -3,5 +3,6 @@ import { Document } from 'mongoose';
 export default interface IUser extends Document {
   email: string,
   username: string,
-  is_logged_in: boolean
+  is_logged_in: boolean,
+  validatePassword: (password: string) => Promise<boolean>
 }
